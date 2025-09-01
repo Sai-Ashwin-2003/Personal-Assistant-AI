e2df1fdccc97ea5409ed8e2919c4b2937e0845f1
Sai Ashwin Kumaar S <121112240+Sai-Ashwin-2003@users.noreply.github.com>
2025-09-01 19:45:58 +0100

    updated imports

--- chat/utils.py
+++ chat/utils.py
@@ -1,4 +1,6 @@
-from langchain.document_loaders import PyPDFLoader, TextLoader, UnstructuredWordDocumentLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import UnstructuredWordDocumentLoader
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def load_document(file_path):
