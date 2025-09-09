df8e5acbf4890971df41d3b638e3dca4bfa5316c
Sai Ashwin Kumaar S <121112240+Sai-Ashwin-2003@users.noreply.github.com>
2025-09-09 06:24:10 +0100

    new chat changes

--- chat/signals.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-from .models import ChatSession
-
-@receiver(user_logged_in)
-def create_chat_session(sender, user, request, **kwargs):
-    ChatSession.objects.create(user=user, title="New Chat")
